44e2e21255e64c920c95608ed7eece44932f410f
Chengyuanyue <131670096+Chengyuanyue@users.noreply.github.com>
2023-06-29 16:01:33 +0800

    Update ETCM-2.py

--- ETCM-2.py
+++ ETCM-2.py
@@ -17,11 +17,13 @@ for k in range(254,402):
     driver = webdriver.Edge("msedgedriver.exe")
     # 访问 url
     driver.get(url)
-#arr = np.empty([0, 2])
+
     # 解析中药成分及 url
     compoundcell = driver.find_elements(By.XPATH, '//table[@id="table"]//tr[@data-index="11"]//a[@class="tdcolor"]')
-    #print(compoundcell)
+
+    #计数
     print(k)
+    
     # 以列表的形式保存数据
     for i in range(0,len(compoundcell)):
         compound = compoundcell[i].text
@@ -31,14 +33,7 @@ for k in range(254,402):
         compounds.append(compound)
         detaileds.append(detailed)
         herb.append(name)
-    #arr_temp = np.array([[compound,detailed]])
-
-    #arr = np.concatenate([arr, arr_temp])
-
-#col_names = ['Compound', 'Detailed']
-# 以 pandas.DataFrame 输出爬到的信息
-#df_result = pd.DataFrame(data=arr)
-#df_result.to_excel('紫菀.xlsx', index=False)
+ 
         # 保存数据
         dataframe=pd.DataFrame({'herb':herb,'compound':compounds,'detailed':detaileds})
-        dataframe.to_excel ("C:/Users/yuechengyuan/Desktop/output_2.xlsx", encoding='utf-8-sig', index=False)
+        dataframe.to_excel ("C:/Users/yuechengyuan/Desktop/ETCM_10421.xlsx", encoding='utf-8-sig', index=False)
